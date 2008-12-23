06c422e7081cd564496185c1d296cd791a7d72c2
Juan P Daza P <tcpip4000@yahoo.com>
2008-12-23 12:41:15 -0500

    crypter encrypts using serpent128

--- crypter.c
+++ crypter.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <gcrypt.h>
 
+#define MAX_DATA_LEN 100
+
 int main()
 {
     const char* libgcrypt_version = gcry_check_version(GCRYPT_VERSION);
@@ -19,11 +21,34 @@ int main()
     // gcry_error_t gcry_cipher_open (gcry cipher hd t *hd, int algo, int
     //         mode, unsigned int flags )
     //
-    printf("hola mundo\n");
+    // Open cipher handler
+    gcry_cipher_hd_t hd;
+    gcry_cipher_open(&hd, GCRY_CIPHER_SERPENT128, GCRY_CIPHER_MODE_OFB, 0);
+
+    //gcry_cipher_setkey
+    //gcry_cipher_setiv
+
+    //gcry_error_t gcry_cipher_setkey (gcry cipher hd t h, const void *k, size t l )
+    //unsigned char *out;
+    char out[MAX_DATA_LEN];
+    char *in = "my secret data";
+    size_t outsize = sizeof(out);
+    size_t insize = sizeof(in);
+    gcry_cipher_encrypt(hd, out, outsize, in, insize);
+
+    printf("entrada: %s\n", in);
+    printf("salida: %s\n", out);
+//gcry_error_t gcry_cipher_encrypt (gcry cipher hd t h, unsigned
+//        char *out, size t outsize, const unsigned char *in, size t inlen )
+//gcry_error_t gcry_cipher_decrypt (gcry cipher hd t h, unsigned
+//        char *out, size t outsize, const unsigned char *in, size t inlen )
+
+    //printf("hola mundo\n");
 
 
     // Release handler
-    //gcry_cipher_close (gcry cipher hd t h )
+    gcry_cipher_close(hd);
+
     return 0;
 	
 }
