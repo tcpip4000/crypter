8b90c7b8a099ca4d4a400f32df44e9362caacd53
Juan P Daza P <tcpip4000@yahoo.com>
2008-12-23 20:32:07 -0500

    Encrypts data from file line by line

--- crypter.c
+++ crypter.c
@@ -8,31 +8,23 @@ int printCharArray(unsigned char *, int);
 
 int main()
 {
-    const char* libgcrypt_version = gcry_check_version(GCRYPT_VERSION);
+    const char *libgcrypt_version = gcry_check_version(GCRYPT_VERSION);
     gcry_error_t err = 0;
     char key[32];
     int keylen;
     unsigned char out[MAX_DATA_LEN], in[MAX_DATA_LEN], plain[MAX_DATA_LEN];
-    char tmp[MAX_REC_LEN];
 
-    memcpy(plain, "foobar42FOOBAR17", 16);
     memcpy(key, "0123456789abcdef.,;/[]{}-=ABCDEF", 32);
+
     FILE *data_file;
     char filename[] = "modal.erc";
 
     if ((data_file = fopen(filename, "r")) == NULL) {
         fprintf(stderr, "Cannot open %s\n", filename);
-        exit(2);
+        exit(1);
     }
 
-    //fgets(tmp, MAX_REC_LEN, data_file);  
-    // printCharArray(tmp, MAX_REC_LEN);
-
-    int l = fread(tmp, MAX_REC_LEN, 1, data_file);
-    printf("length read %d\n", l);
-    printCharArray(tmp, MAX_REC_LEN);
-    
-    // libgcrypt initialization
+    // cipher initialization
     if (!libgcrypt_version) {
         fputs ("libgcrypt version mismatch\n", stderr);
         exit (2);
@@ -40,47 +32,43 @@ int main()
         printf("Using libgcrypt version: %s\n", libgcrypt_version);
     }
 
-    // Initialization complete
+    // cipher settings
     gcry_control(GCRYCTL_INITIALIZATION_FINISHED, 0);
-
     keylen = gcry_cipher_get_algo_keylen(GCRY_CIPHER_SERPENT128);
-    printf("keylen: %d\n" , keylen);
-    // main actions
-    // gcry_error_t gcry_cipher_open (gcry cipher hd t *hd, int algo, int
-    //         mode, unsigned int flags )
-    //
+    
     // Open cipher handler
     gcry_cipher_hd_t hd;
     gcry_cipher_open(&hd, GCRY_CIPHER_SERPENT128, GCRY_CIPHER_MODE_OFB, 0);
 
     err = gcry_cipher_setkey(hd, key, keylen);
     //gcry_cipher_setiv
+    
+    while (fgets(plain, MAX_REC_LEN, data_file) != NULL) {
+    
+        err = gcry_cipher_encrypt(hd, out, MAX_DATA_LEN, plain, MAX_DATA_LEN);
+        gcry_cipher_reset(hd);
 
-    //gcry_error_t gcry_cipher_setkey (gcry cipher hd t h, const void *k, size t l )
-    err = gcry_cipher_encrypt(hd, out, MAX_DATA_LEN, plain, MAX_DATA_LEN);
+        printf("entrada: ");
+        printCharArray(plain, MAX_DATA_LEN);
+        printf("salida:  ");
+        printCharArray(out, MAX_DATA_LEN);
 
-    printf("entrada: ");
-    printCharArray(plain, MAX_DATA_LEN);
-    printf("salida:  ");
-    printCharArray(out, MAX_DATA_LEN);
+        err = gcry_cipher_decrypt(hd, in, MAX_DATA_LEN, out, MAX_DATA_LEN);
+        gcry_cipher_reset(hd);
 
-    gcry_cipher_reset(hd);
-
-    err = gcry_cipher_decrypt(hd, in, MAX_DATA_LEN, out, MAX_DATA_LEN);
-
-    printf("entrada: ");
-    printCharArray(out, MAX_DATA_LEN);
-    printf("salida:  ");
-    printCharArray(in, MAX_DATA_LEN);
+        printf("entrada: ");
+        printCharArray(out, MAX_DATA_LEN);
+        printf("salida:  ");
+        printCharArray(in, MAX_DATA_LEN);
+    }
 
     //gcry_error_t gcry_cipher_encrypt (gcry cipher hd t h, unsigned
     //        char *out, size t outsize, const unsigned char *in, size t inlen )
     //gcry_error_t gcry_cipher_decrypt (gcry cipher hd t h, unsigned
     //        char *out, size t outsize, const unsigned char *in, size t inlen )
 
-    // Release handler
+    // Release file and handler
     gcry_cipher_close(hd);
-
     fclose(data_file);
 
     return 0;
@@ -90,11 +78,8 @@ int main()
 int printCharArray(unsigned char *array, int array_length)
 {
     int i;
-    //for (i=0; i < array_length && array[i] != '\n'; i++) {
-    for (i=0; i < array_length; i++) {
+    for (i=0; i < array_length && array[i] != NULL; i++) {
         printf("%c", array[i]);
-        //if (array[i] == NULL)
-        //    printf("Null encontrado");
     }
     printf("\n");
     return 0;
